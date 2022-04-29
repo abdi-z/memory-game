fcded9b50ea26a730183461d704020393beb0d72
Abdul Rehman Aziz <fa19-bcs-016@cuilahore.edu.pk>
2022-04-30 03:52:42 +0500

    ...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import './App.css'
+import "./App.css";
+import React from "react";
+
+const cardImages = [
+  { src: "/img/cover.png", id: 0 },
+  { src: "/img/helmet-1.png", id: 1 },
+  { src: "/img/potion-1.png", id: 2 },
+  { src: "/img/ring-1.png", id: 3 },
+  { src: "/img/shield-1.png", id: 4 },
+  { src: "/img/sword-1.png", id: 5 },
+  { src: "/img/scroll-1.png", id: 6 },
+];
 
 function App() {
+  
+  const [cards,setCards] = React.useState([])
+
+  const shuffleCards =() => {
+    const shuffledCards =[...cardImages, ...cardImages]
+    .sort(()=>Math.random()-0.5)
+    .map((card)=>({...card, id:Math.random()}))
+
+    setCards(shuffledCards)
+  }
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
       <button>New Game</button>
+      <div className="cards">
+
+        </div>
+
     </div>
   );
 }
 
-export default App
+export default App;
